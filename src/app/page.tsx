c9c72952c2282506aec6d98b5ccf5fa64fc732fc
NicolasKmbr <braun.nicolasbraun@gmail.com>
2023-06-06 17:37:18 +0200

    feature: component for form text input added. Placeholder Login Page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import burger from "../../public/burger-menu.svg";
+import { FC, ReactNode } from "react";
+import { FormTextInput } from "@/components/FormInput";
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="w-screen h-screen bg-orange-200 p-4">
+      <div className="flex flex-col items-center bg-slate-500 bg-opacity-50 w-full h-full rounded-md">
+        <div className="border-4 border-amber-300 rounded-sm">
+          <Image src={burger} alt="test" width={200} height={200} />
+        </div>
+        <form className="flex flex-col mx-4" action="">
+          <FormTextInput label="email" placeholder="username@gmail.com">
+            Email
+          </FormTextInput>
+          <FormTextInput label="password" placeholder="*******">
+            Password
+          </FormTextInput>
+          <button
+            className="text-amber-300 bg-white rounded-sm mt-3"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
