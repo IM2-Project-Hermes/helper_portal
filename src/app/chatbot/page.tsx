b1c32383fd863de863a534b4b0120e492a45a04e
Nicolas Braun <76167936+NicolasKmbr@users.noreply.github.com>
2023-07-23 13:52:43 +0200

    feature: Real Api integrated with the search functionality

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,12 +1,11 @@
-import BoxContainer from '@/components/BoxContainer';
-
+import BotContainer from "@/components/BoxContainer";
 
 const ChatbotPage = () => {
   return (
-    <div className='h-screen pt-3'>
-    <BoxContainer />
+    <div className="h-screen pt-3">
+      <BotContainer />
     </div>
-  )
-}
+  );
+};
 
 export default ChatbotPage;
---
+++ src/components/BoxContainer.jsx
@@ -0,0 +1,103 @@
+"use client";
+import { useState, useEffect } from "react";
+import lupe from "../../public/lupe.svg";
+import styles from "./BoxContainer.module.css";
+import Image from "next/image";
+
+const BotContainer = () => {
+  const [question, setQuestion] = useState("");
+
+  const handleDataChange = (newQuestion) => {
+    setQuestion(newQuestion);
+  };
+
+  return (
+    <>
+      <SearchContainer onQuestionChange={handleDataChange} />
+      <AnswerContainer question={question} />
+    </>
+  );
+};
+
+export const SearchContainer = ({ onQuestionChange }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onQuestionChange(inputValue);
+  };
+
+  return (
+    <div className={styles.boxContainer}>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-between rounded-md pl-3 h-10 bg-white"
+      >
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your question..."
+        />
+        <button className="h-10 w-10 p-1" type="submit">
+          <Image src={lupe} alt="test" />
+        </button>
+      </form>
+      <p>Submitted Question: {inputValue}</p>
+    </div>
+  );
+};
+
+export const AnswerContainer = ({ question }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://europe-west3-project-hermes-390519.cloudfunctions.net/api?question=[${question}]`
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [question]);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className={styles.boxContainer}>
+        <div className="flex flex-col gap-4 rounded-md p-3 bg-white">
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {data ? (
+                <>
+                  <p>{data.result.answer}</p>
+                  <a href="">{data.result.sources}</a>
+                </>
+              ) : (
+                <p>No data available.</p>
+              )}
+            </>
+          )}
+        </div>
+        <p>Question sent to ChatBot: {question}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BotContainer;
